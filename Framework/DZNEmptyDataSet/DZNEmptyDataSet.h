0e3b2f83670c491e70828e644c743e48b9ce7d78
Jake Marsh <jake@thejakemarsh.com>
2015-05-31 03:04:19 -0700

    Add framework project/target and shared scheme to support Carthage.

---
+++ Framework/DZNEmptyDataSet/DZNEmptyDataSet.h
@@ -0,0 +1,19 @@
+//
+//  DZNEmptyDataSet.h
+//  DZNEmptyDataSet
+//
+//  Created by Jake Marsh on 5/31/15.
+//  Copyright (c) 2015 Magnus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DZNEmptyDataSet.
+FOUNDATION_EXPORT double DZNEmptyDataSetVersionNumber;
+
+//! Project version string for DZNEmptyDataSet.
+FOUNDATION_EXPORT const unsigned char DZNEmptyDataSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DZNEmptyDataSet/PublicHeader.h>
+
+
